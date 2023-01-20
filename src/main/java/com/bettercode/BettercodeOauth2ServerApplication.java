4284c7bae8b5ff76a9ca524e7d0206badae0ec95
beco <galashow7@helixtech.co.kr>
2023-01-20 10:41:36 +0900

    Initial commit

---
+++ src/main/java/com/bettercode/BettercodeOauth2ServerApplication.java
@@ -0,0 +1,13 @@
+package com.bettercode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BettercodeOauth2ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BettercodeOauth2ServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bettercode/BettercodeOauth2ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bettercode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BettercodeOauth2ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
